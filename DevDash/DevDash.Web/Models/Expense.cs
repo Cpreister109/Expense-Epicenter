725d805d1f7a3b2aa0fabe74d2ea4c24ecf4b62f
Cade Preister <cadepreister@unomaha.edu>
2024-08-24 18:00:10 -0500

    Initial commit

---
+++ DevDash/DevDash.Web/Models/Expense.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevDash.Web.Models
+{
+	public class Expense
+	{
+		public int Id { get; set; }
+		public decimal Value { get; set; }
+
+		[Required]
+		public string? Description { get; set; }
+	}
+}
+
---
+++ DevDash/DevDash.Web/Models/devDashDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DevDash.Web.Models
+{
+	public class DevDashDbContext : DbContext
+    {
+		public DbSet<Expense> Expenses { get; set; }
+
+		public DevDashDbContext(DbContextOptions<DevDashDbContext> options)
+			: base(options)
+		{
+
+		}
+    }
+}
+
