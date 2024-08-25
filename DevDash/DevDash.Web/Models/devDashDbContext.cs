3d1f15dc342668c6c749169518858c1501c00b3b
Cade Preister <cadepreister@unomaha.edu>
2024-08-24 23:10:28 -0500

    Integrated SQL

--- DevDash/DevDash.Web/Models/devDashDbContext.cs
+++ DevDash/DevDash.Web/Models/devDashDbContext.cs
@@ -4,13 +4,31 @@ namespace DevDash.Web.Models
 {
 	public class DevDashDbContext : DbContext
     {
-		public DbSet<Expense> Expenses { get; set; }
-
 		public DevDashDbContext(DbContextOptions<DevDashDbContext> options)
 			: base(options)
 		{
 
 		}
+
+        public DbSet<Expense> Expenses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Expense>()
+                .HasKey(e => e.Id);
+
+            modelBuilder.Entity<Expense>()
+                .Property(e => e.Description)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            modelBuilder.Entity<Expense>()
+                .Property(e => e.Value)
+                .HasColumnType("decimal(18,2)")
+                .IsRequired();
+        }
     }
 }
 
--- DevDash/DevDash.Web/Program.cs
+++ DevDash/DevDash.Web/Program.cs
@@ -6,8 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<DevDashDbContext>(options =>
-    options.UseInMemoryDatabase("devDashDb")
-);
+    options.UseSqlite("Data Source=devdash.db"));
 
 var app = builder.Build();
 
@@ -15,7 +14,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
